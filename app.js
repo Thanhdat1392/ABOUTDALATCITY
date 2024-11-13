8eb769e3a99f6be0bfd3a7b1f90de98b5e4d9a08
Thanhdat1392 <108585183+Thanhdat1392@users.noreply.github.com>
2024-11-13 17:42:23 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+$('.image-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows:false,
+  });
